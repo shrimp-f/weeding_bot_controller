03db3bd6d97a2579a5d1c4f554a706282fba41ed
shrimp-f <fujitadynamics@gmail.com>
2019-08-17 18:59:02 +0900

    LQRと閾値のハイブリッド。

--- scripts/LQR_controller_cmd_vel_type.py
+++ scripts/LQR_controller_cmd_vel_type.py
@@ -10,18 +10,20 @@ import numpy as np
 # import scipy.linalg as la
 import math
 
-center_distance = 0.
-ridge_angle = 0.
 
 # ロボットの前進速度
-CONST_X_LINEAR = 1.5
+CONST_X_LINEAR = 0.5
+
+# delta_t 実行時間間隔dt rostopic hz /estimator_linear/center_distance から
+delta_t = 1. / 21
+
 
 #### LQR 関係 #################
 # x[k+1] = Ax[k] + Bu[k]
 A = np.matrix([[1.0, 0], 
                [0, 1.0]])
-B = np.matrix([[0.1, CONST_X_LINEAR],
-              [0.0, 1.0]])
+B = np.matrix([[0., 0],
+              [0.0, -1.0]])
 C = np.matrix([1.0, 0.0])
 Kopt = None
 #### LQR 関係 おわり#################
@@ -62,8 +64,10 @@ def dlqr(A, B, Q, R):
 
 def lqr_control(x):
     global Kopt
-    B = np.matrix([[x[1,0], 0.0],
-                    [0.0, 1.0]])
+#    B = np.matrix([[x[1,0]*delta_t + 1.0, 0.0],
+#                    [0.0, -1.0]])
+    B = np.matrix([[0.0, 0.0],
+                    [0.0, -1.0]])
 
     # Kopt毎回計算する
 #    Kopt, X, ev = dlqr(A, B, C.T * np.eye(2) * C, np.eye(2))
@@ -85,37 +89,58 @@ def lqr_control(x):
 
 
 def callback_distance(msg):
+    global center_distance
     center_distance = msg.data
-    rospy.loginfo("center_distance: %f", center_distance)
+#    rospy.loginfo("center_distance: %f", center_distance)
 
 
 def callback_angle(msg):
+    global ridge_angle
     ridge_angle = msg.data
     rospy.loginfo("ridge_angle: %f", ridge_angle)
 
-    # 状態変数
-    x = np.matrix([center_distance, ridge_angle]).T # [y, theta]
-    u = np.matrix([0.0, 0.0]).T # [x_l, z_a]
-#    B = np.matrix([[x[1,0], 0.0],
-#                    [0.0, 1.0]])
-    B = np.matrix([[0.3, 0.0],
-                    [0.0, 1.0]])
+    rospy.loginfo("center_distance: %f", center_distance)
 
-    # LQRで最適な入力を計算
-    u = lqr_control(x)
-    
     # 速度をセット
     move_cmd = Twist()
-    move_cmd.linear.x = u[0,0]*10 + CONST_X_LINEAR
-    handle_a = 0
-    if center_distance > 10:
-        handle_a = 3
-    elif center_distance < -10:
-        handle_a = - 3
-    move_cmd.angular.z = -u[1,0] * 10 + handle_a
-#    move_cmd.linear.x = CONST_X_LINEAR
-#    move_cmd.angular.z = 1.5
-    rospy.loginfo("u0: %f", u[0,0])
+
+    # 切り替えの閾値
+    global threshold
+    threshold = 30
+    T1 = 30
+    T2 = 15
+
+    if abs(center_distance) >= threshold:
+        threshold = T2
+        # 速度をセット
+        if center_distance < 0:
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = 0.5 
+        elif center_distance > 0:
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = -0.5 
+
+
+    elif abs(center_distance) < threshold: 
+        threshold = T1
+        # 状態変数
+        x = np.matrix([center_distance, ridge_angle]).T # [y, theta]
+        u = np.matrix([0.0, 0.0]).T # [x_l, z_a]
+    #    B = np.matrix([[x[1,0], 0.0],
+    #                    [0.0, 1.0]])
+        B = np.matrix([[0.3, 0.0],
+                        [0.0, 1.0]])
+
+        # LQRで最適な入力を計算
+        u = lqr_control(x)
+        
+        # 速度をセット
+        move_cmd.linear.x = u[0,0]*10 + CONST_X_LINEAR
+        move_cmd.angular.z = u[1,0] * 10 
+    #    move_cmd.linear.x = CONST_X_LINEAR
+    #    move_cmd.angular.z = 1.5
+        rospy.loginfo("u0: %f", u[0,0])
+
 
     # twist型をpublish
     pub = rospy.Publisher('/my_robo_two/diff_drive_controller/cmd_vel', Twist, queue_size=3)
@@ -133,5 +158,6 @@ def listener():
 
 
 if __name__ == '__main__':
+
     listener()
 
